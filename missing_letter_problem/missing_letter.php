6e9c5ff9cbac70c6f7b82d448d36ee360f04eaa2
AbdelrahmanSameh1 <abdelrhmansameh92@gmail.com>
2022-07-02 19:08:52 +0200

    first

---
+++ missing_letter_problem/missing_letter.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function find_missing_letter_in($givenLetters)
+{
+}
+
+
+
+
+
+// Testing Output
+
+echo find_missing_letter_in("abcdeghi") . "<br>"; // f
+echo find_missing_letter_in("defgi") . "<br>"; // h
+echo find_missing_letter_in("xyz") . "<br>"; // no missing letters in sequence
