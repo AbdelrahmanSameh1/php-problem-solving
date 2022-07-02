46efc85fa9003e2de4cfc8be0b13c230c9f9bc98
AbdelrahmanSameh1 <abdelrhmansameh92@gmail.com>
2022-07-02 19:14:53 +0200

    edit

--- missing_letter_problem/missing_letter.php
+++ missing_letter_problem/missing_letter.php
@@ -3,6 +3,21 @@
 
 function find_missing_letter_in($givenLetters)
 {
+
+    $alpha = '';
+
+    foreach (range("a", "z") as $letter) {
+        $alpha .= $letter;
+    }
+
+    $start = strpos($alpha, $givenLetters[0]);
+
+    for ($i = 0; $i < strlen($givenLetters); $i++) {
+        if ($alpha[$start + $i] != $givenLetters[$i]) {
+            return $alpha[$start + $i];
+        }
+    }
+    return "no missing letters in sequence";
 }
 
 
@@ -11,6 +26,6 @@ function find_missing_letter_in($givenLetters)
 
 // Testing Output
 
-echo find_missing_letter_in("abcdeghi") . "<br>"; // f
-echo find_missing_letter_in("defgi") . "<br>"; // h
+// echo find_missing_letter_in("abcdeghi") . "<br>"; // f
+// echo find_missing_letter_in("defgi") . "<br>"; // h
 echo find_missing_letter_in("xyz") . "<br>"; // no missing letters in sequence
