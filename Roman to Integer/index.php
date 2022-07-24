dd46b1158cddd527426b85824e45c55c2af04ccf
AbdelrahmanSameh1 <abdelrhmansameh92@gmail.com>
2022-07-24 16:40:06 +0200

    new problem

---
+++ Roman to Integer/index.php
@@ -0,0 +1,39 @@
+<?php
+
+function romanToInt($s)
+{
+
+    $arr = [
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    ];
+
+    $strArr = str_split($s);
+    $strArrLen = count($strArr);
+
+    // echo $arr[$strArr[0]];
+    // die;
+
+
+    $res = $arr[$strArr[$strArrLen - 1]];
+    // $res = 0;
+
+    for ($i = 0; $i < $strArrLen - 1; $i++) {
+        if ($arr[$strArr[$i]] < $arr[$strArr[$i + 1]]) {
+            $res = $res - $arr[$strArr[$i]];
+        } else {
+            $res = $res + $arr[$strArr[$i]];
+        }
+    }
+
+    return $res;
+}
+
+
+
+romanToInt("MCMXCIV");
