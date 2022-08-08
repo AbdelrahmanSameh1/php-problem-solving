a1a3973f01bc03f78206f70e23dde79f32ce2067
AbdelrahmanSameh1 <abdelrhmansameh92@gmail.com>
2022-08-08 20:34:03 +0200

    new problem

---
+++ Palindrome Number/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+function isPalindrome($n)
+{
+
+    $arr = str_split($n);
+    $arrLen = count($arr);
+
+    for ($i = 0, $j = $arrLen - 1; $i < $j; $i++, $j--) {
+
+        if ($arr[$i] != $arr[$j]) {
+            return "false";
+        }
+    }
+
+    return "true";
+}
+
+
+
+echo isPalindrome(12321);
